1de2adae17ba74da69215f6eb7b6e0b78103417b
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-01-19 21:36:52 +0100

    Delete appcomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -18,7 +17,6 @@ import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
-    AppComponent,
     NavComponent,
     HomeComponent,
     NotFoundComponent,
