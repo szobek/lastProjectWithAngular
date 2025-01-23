6f08b495b268a1650e6571ac9c1bd588d136c310
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-01-23 15:50:00 +0100

    If the user is undefined navigate app to user list

--- src/app/components/users/single-user/single-user.component.ts
+++ src/app/components/users/single-user/single-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Post } from 'src/app/models/Post';
 import { User } from 'src/app/models/User';
 import { CallService } from 'src/app/services/call.service';
@@ -12,6 +12,7 @@ import { CallService } from 'src/app/services/call.service';
 export class SingleUserComponent {
 callService=inject(CallService)
 activatedRoute=inject(ActivatedRoute)
+router=inject(Router)
 selectedUser!:User
 id!:number
 userPosts!:Post[]
@@ -22,6 +23,7 @@ ngOnInit() {
       next:(users:User[]|null)=>{
         if(users){
           this.selectedUser=users.filter((user:User)=>{return user.id===this.id})[0]
+          if(this.selectedUser==undefined) this.router.navigate(["/users"])
           document.title=this.selectedUser.name
           this.userPosts=this.callService.$posts.value.filter((post:Post)=>{return post.userId===this.selectedUser.id})
         }
