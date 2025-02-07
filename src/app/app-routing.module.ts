ae2da63a316c426a21581bbe4875d3444d60eba7
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-02-07 19:14:38 +0100

    Add coffee routing module and lazy load coffee modulle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HomeComponent } from './components/home/home.component';
-import { StatisticsWrapperComponent } from './components/statistics/wrapper/statistics-wrapper.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent,title:"Home"},
+  {path: 'coffees', loadChildren: () => import('./modules/coffee/coffee/coffee.module').then(m => m.CoffeeModule)},
   {path: '**', component: NotFoundComponent,pathMatch: 'full',title:"Not found"},
 ];
 
--- src/app/modules/coffee/coffee-routing/coffee-routing.module.ts
+++ src/app/modules/coffee/coffee-routing/coffee-routing.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { CoffeeListComponent } from 'src/app/components/coffee/coffee-list/coffee-list.component';
 import { SingleCoffeeComponent } from 'src/app/components/coffee/single-coffee/single-coffee.component';
-import { SharedModule } from '../../shared/shared.module';
-import { CoffeeRoutingModule } from '../coffee-routing/coffee-routing.module';
 
-
+const routes: Routes = [
+  {path: 'coffees', component:CoffeeListComponent,title:'Coffee list'},
+  {path: 'coffee/:id', component:SingleCoffeeComponent},
+]
 
 @NgModule({
-  declarations: [
-    CoffeeListComponent,
-    SingleCoffeeComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
-    SharedModule,
-    CoffeeRoutingModule
+    RouterModule.forRoot(routes)
   ],
   exports: [
-    CoffeeListComponent,
-    SingleCoffeeComponent
+    RouterModule
   ]
 })
-export class CoffeeModule { }
+export class CoffeeRoutingModule { }
