d7eb4525fe8c83619387ee7ec757a8fc4bed7040
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-02-08 16:42:06 +0100

    Remove unused BrowserModule import from task module

--- src/app/modules/task/task.module.ts
+++ src/app/modules/task/task.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TaskListComponent } from 'src/app/components/task/task-list/task-list.component';
 import { SharedModule } from '../shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { TaskRoutingModule } from './task-routing.module';
 
 
